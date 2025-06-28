39cceb6508b4824b67f3306a29e8c6393d7900ce
Jonatan Borkowski <jonatan@thebo.me>
2025-06-28 19:27:41 +0200

    Initial commit with esp-hal 0.22

---
+++ README.md
@@ -0,0 +1,20 @@
+# ESP E-Ink Weather Station
+
+### ESP Rust Toolchain Setup
+
+This project uses the ESP Rust toolchain. To set up the required toolchain:
+
+1. Install `espup` if you haven't already:
+   ```bash
+   cargo install espup
+   ```
+
+2. Install the ESP toolchain version 1.79.0:
+   ```bash
+   espup install --toolchain-version=1.79.0 --name esp-1.79
+   ```
+
+3. Load the ESP environment variables in your current shell:
+   ```bash
+   . $HOME/export-esp.sh
+   ```
---
+++ src/bin/async_main.rs
@@ -0,0 +1,81 @@
+#![no_std]
+#![no_main]
+
+use embedded_graphics::pixelcolor::Gray4;
+use esp_backtrace as _;
+use esp_hal::{delay::Delay, prelude::*};
+use lilygo_epd47::{pin_config, Display, DrawMode};
+use log::info;
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+
+extern crate alloc;
+
+#[main]
+async fn main(spawner: Spawner) {
+    let peripherals = esp_hal::init({
+        let mut config = esp_hal::Config::default();
+        config.cpu_clock = CpuClock::max();
+        config
+    });
+
+    esp_alloc::heap_allocator!(72 * 1024);
+
+    esp_println::logger::init_logger_from_env();
+
+    let timer0 = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER)
+        .split::<esp_hal::timer::systimer::Target>();
+    esp_hal_embassy::init(timer0.alarm0);
+
+    info!("Embassy initialized!");
+
+    let timer1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
+    let _init = esp_wifi::init(
+        timer1.timer0,
+        esp_hal::rng::Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+    )
+    .unwrap();
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    // Create PSRAM allocator
+    esp_alloc::psram_allocator!(peripherals.PSRAM, esp_hal::psram);
+
+    let mut display = Display::new(
+        pin_config!(peripherals),
+        peripherals.DMA,
+        peripherals.LCD_CAM,
+        peripherals.RMT,
+    )
+    .expect("Failed to initialize display");
+    info!("EPD47 initialized!");
+
+    let delay = Delay::new();
+
+    delay.delay_millis(100);
+    display.power_on();
+    delay.delay_millis(10);
+    display.clear().unwrap();
+
+    use embedded_graphics::{
+        prelude::*,
+        primitives::{Circle, PrimitiveStyle},
+    };
+
+    Circle::new(display.bounding_box().center() - Point::new(100, 100), 200)
+        .into_styled(PrimitiveStyle::with_stroke(Gray4::BLACK, 3))
+        .draw(&mut display)
+        .unwrap();
+    display.flush(DrawMode::BlackOnWhite).unwrap();
+    display.power_off();
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.22.0/examples/src/bin
+}
